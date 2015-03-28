46e6740cf79cd4acb45ef507e9ce7eebe7c7a44c
Krzysztof Misztal <krmisztal@gmail.com>
2015-03-28 15:47:17 +0100

    Changed Queue capacity to 5

--- src/com/misztal/producerconsument/Runner.java
+++ src/com/misztal/producerconsument/Runner.java
@@ -2,13 +2,12 @@ package com.misztal.producerconsument;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by misztal on 28.03.15.
  */
 public class Runner {
-    private static final int QUEUE_CAPACITY = 10;
+    private static final int QUEUE_CAPACITY = 5;
 
     public static void main(String[] args) {
         BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<Integer>(QUEUE_CAPACITY);
